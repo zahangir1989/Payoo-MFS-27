a293b0d2277f4da9fccebc25e2fc181e30d72cab
zahangir <alamzahangir218@gmail.com>
2025-04-16 00:50:34 +0600

    already verify section is done

---
+++ js/cashout.js
@@ -0,0 +1,26 @@
+// document.getElementById('btn-cash-out').addEventListener('click', function(event){
+// event.preventDefault();
+// // console.log('cash out clicked')
+
+// const cashOut = document.getElementById('cash-out-money').value;
+// const cashOutFloat = parseFloat(cashOut);
+// const cashOutPin = document.getElementById('cash-out-pin').value;
+// console.log(cashOut, cashOutPin)
+
+// // varify pin
+// if(cashOutPin === '1234'){
+//  console.log('money is reducing')
+//  const balance = document.getElementById('balance').innerText;
+//  console.log(balance)
+//  const balanceFloat = parseFloat(balance);
+
+// //  reduce the balance
+//  const newBalance = balanceFloat - cashOutFloat;
+
+// //  update the balance
+// document.getElementById('balance').innerText = newBalance
+// }
+// else{
+//     alert('failed to cash out.Please try again later.')
+// }
+// })
--- js/home.js
+++ js/home.js
@@ -1,11 +1,34 @@
 
-// add event handler
+//1 add event handler
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
 
-//    add inpur money
+// 2   add input money
 const addInputMoney = document.getElementById('add-input-money').value;
+console.log(addInputMoney);
+
 const addInputPin =document.getElementById('input-pin').value;
-console.log(addInputMoney, addInputPin);
+console.log(addInputPin);
+
+//3  verify pin number
+
+ if(addInputPin === '1234'){
+    console.log('adding money to you account')
+// 4   get the current balance
+ const balance = document.getElementById('balance').innerText;
+ console.log(balance)
+//5  add money
+ const addMoney = parseFloat(addInputMoney);
+ const addBalance= parseFloat(balance);
+ const newBalance = addMoney+addBalance;
+ console.log(newBalance);
+
+//  6 account update
+document.getElementById('balance').innerText=newBalance;
+
+ }
+ else{
+    alert('failed to add money! Please tr again.')
+ }
 
 })
