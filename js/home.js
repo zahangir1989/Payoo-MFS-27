6db77258054bd7c15099ee0fd8ad0f204d1aef33
zahangir <alamzahangir218@gmail.com>
2025-04-15 16:04:22 +0600

    input pin is add

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home is added')
+
+// add event handler
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+//    add inpur money
+const addInputMoney = document.getElementById('add-input-money').value;
+const addInputPin =document.getElementById('input-pin').value;
+console.log(addInputMoney, addInputPin);
+
+})
