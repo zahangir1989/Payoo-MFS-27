0e96889c55426e9a371317f7c52d4ad8c1451109
zahangir <alamzahangir218@gmail.com>
2025-04-15 15:35:19 +0600

    add money is addded

---
+++ js/home.js
@@ -0,0 +1,1 @@
+console.log('home is added')
--- js/login.js
+++ js/login.js
@@ -1,6 +1,19 @@
 
 document.getElementById('button').addEventListener('click', function(event){
    event.preventDefault();
+
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById('pin-number').value;
+  console.log(phoneNumber, pinNumber)
+
+  if(phoneNumber==='5' && pinNumber==='1234'){
+    console.log('you are login now')
+   window.location.href = '/home.html';
+  }
+  else{
+    alert('wrong phone number or pin')
+  }
+
+
+
 })
