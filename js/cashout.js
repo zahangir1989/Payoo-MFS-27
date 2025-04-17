9b1052f45867938fb2302fc7c786e95285aa687d
zahangir <alamzahangir218@gmail.com>
2025-04-17 23:25:02 +0600

    cashout ready to use

--- js/cashout.js
+++ js/cashout.js
@@ -1,6 +1,5 @@
 document.getElementById('btn-cash-out').addEventListener('click', function(event){
 event.preventDefault();
-// console.log('cash out clicked')
 
 const cashOut = document.getElementById('cash-out-money').value;
 const cashOutFloat = parseFloat(cashOut);
