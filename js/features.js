1e51e36e66a5d486b4ea95f01de245bac6d8a2ab
zahangir <alamzahangir218@gmail.com>
2025-04-17 13:51:24 +0600

    some feature are work now

--- js/features.js
+++ js/features.js
@@ -1,20 +1,20 @@
 
 
-// // show the cash out form
+// show the cash out form
 
-// document.getElementById('btn-show-cash-out').addEventListener('click', function(){
-//     console.log('show cash out button clicked')
+document.getElementById('btn-show-cash-out').addEventListener('click', function(){
+    console.log('show cash out button clicked')
 
-//     // show cash out button clicked
-//     document.getElementById('cash-out-form').classList.remove('hidden');
+    // show cash out button clicked
+    document.getElementById('cash-out-form').classList.remove('hidden');
 
-//     // hide the add money form
-//     document.getElementById('add-money-form').classList.add('hidden')
-// })
+    // hide the add money form
+    document.getElementById('add-money-form').classList.add('hidden')
+})
 
-// // show add money form and hide the cash outform
+// show add money form and hide the cash outform
 
-// document.getElementById('btn-show-add-money').addEventListener('click', function(){
-//     document.getElementById('add-money-form').classList.remove('hidden');
-//     document.getElementById('cash-out-form').classList.add('hidden');
-// })
+document.getElementById('btn-show-add-money').addEventListener('click', function(){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+})
